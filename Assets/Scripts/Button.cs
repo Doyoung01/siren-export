4231f7f8ac21885cb66259d064af92de3340ca3b
Doyoung01 <jjyo0108@gmail.com>
2024-05-05 02:43:54 +0900

    Make Pause Function

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public GameObject button;
+    public InputActionAsset actionAsset;
+
+    public Text textUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var rightAButton = actionAsset.actionMaps[5].actions[0].ReadValue<float>();
+
+        textUI.text = rightAButton.ToString();
     }
 }
