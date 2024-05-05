3db81b597ad7816cbef904aaea22e114b41ce1b9
Doyoung01 <74997144+Doyoung01@users.noreply.github.com>
2024-05-05 17:25:14 +0900

    Revert "Test merge"

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,14 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public InputActionAsset actionAsset;
-
-    public Text textUI;
+    public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +14,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var rightAButton = actionAsset.actionMaps[5].actions[0].ReadValue<float>();
-
-        textUI.text = rightAButton.ToString();
+        
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 using System.Threading;
 using Unity.VisualScripting;
 using System.Data;
@@ -43,12 +42,6 @@ public class GameManager : MonoBehaviour
     public GameObject clear; // 클리어창
     public GameObject totalInfo;
 
-    [Header("Pause")]
-    public InputActionAsset actionAsset;
-    public GameObject pauseWindow;
-    private bool isPause;
-    float saveButton = 0;
-
     public bool isActiveInfo()
     {
         return infoWindows.activeSelf;
@@ -67,13 +60,11 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isPause = false;
         RestartButton.SetActive(false);
         fires.SetActive(false);
         clear.SetActive(false);
         totalInfo.SetActive(false);
         infoWindows.SetActive(false);
-        pauseWindow.SetActive(false);
         audio = GetComponent<AudioSource>();
         countScript = GetComponent<Objectcount>();
     }
@@ -122,41 +113,14 @@ public class GameManager : MonoBehaviour
         // audio.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / spd);
     }
 
-
     // Update is called once per frame
     void Update()
     {
+
         // Player.transform.position = new Vector3(0, 6, 0);
 
         if (isChecked == true){
-            var rightAButton = actionAsset.actionMaps[5].actions[0].ReadValue<float>();
-            
-            if (rightAButton == 1)
-            {
-                saveButton = 1;
-            } else
-            {
-                if(isPause == true && pauseWindow.activeSelf == false)
-                {
-                    saveButton = 0;
-                }
-            }
-            if (isPause == false && saveButton == 1)
-            {
-                pauseWindow.SetActive(true);
-                Time.timeScale = 0;
-                isPause = true;
-                return;
-            }
-            else if (isPause == true && pauseWindow.activeSelf == false)
-            {
-                Time.timeScale = 1;
-                isPause = false;
-                return;
-            }
-
-
-            if (!hasAppeared && isPause == false) {
+            if (!hasAppeared) {
                 if (countScript.getCount() == countScript.getObcount())
                 {
                     isChecked = false;
@@ -213,5 +177,13 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
+        
+        
+
+        
     }
+
+
+
+
 }
